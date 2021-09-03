b186957856f85b0af3f546383263cb1480bf23a9
B9930402E9DA2861214F2B7D3E8921EC <B9930402E9DA2861214F2B7D3E8921EC@i-search.com.cn>
2021-09-03 20:29:21 +0800

    .

---
+++ src/main/java/com/songqingchao/aop/SqcAop.java
@@ -0,0 +1,14 @@
+package com.songqingchao.aop;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+/**
+ * @user candy
+ * 2021/8/2819:21
+ */
+@Aspect
+@Component
+public class SqcAop {
+
+}
--- src/main/java/com/songqingchao/controller/XinwenController.java
+++ src/main/java/com/songqingchao/controller/XinwenController.java
@@ -1,6 +1,7 @@
 package com.songqingchao.controller;
 
 import com.songqingchao.servcie.PingFenService;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,12 +17,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class XinwenController {
     @Autowired
     private PingFenService pingFenService;
+    @Autowired
+    private SqlSessionTemplate sqlSessionTemplate;
     /**
      * 服务对象
      */
 
     @RequestMapping("/hello")
     public String xiwenhello(){
+        System.out.println(sqlSessionTemplate.getSqlSessionFactory());
         return "hello,苏祁";
     }
 
