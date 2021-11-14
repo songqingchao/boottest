b99bec024ed918c1b2b3272d928c7c14a8fef175
B9930402E9DA2861214F2B7D3E8921EC <B9930402E9DA2861214F2B7D3E8921EC@i-search.com.cn>
2021-11-14 23:57:13 +0800

    .

--- src/main/java/com/songqingchao/controller/XinwenController.java
+++ src/main/java/com/songqingchao/controller/XinwenController.java
@@ -3,6 +3,7 @@ package com.songqingchao.controller;
 import com.songqingchao.servcie.PingFenService;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,13 +20,15 @@ public class XinwenController {
     private PingFenService pingFenService;
     @Autowired
     private SqlSessionTemplate sqlSessionTemplate;
+    @Autowired
+    private ApplicationContext applicationContext;
     /**
      * 服务对象
      */
 
     @RequestMapping("/hello")
-    public String xiwenhello(){
-        System.out.println(sqlSessionTemplate.getSqlSessionFactory());
+    public String xiwenhello() throws Exception {
+        System.out.println(applicationContext.getBean("sqlSessionFactory"));
         return "hello,苏祁";
     }
 
